a3c1534a0de222983f2e07a59da047cd920842d6
Emma1987 <mercadal.em@gmail.com>
2018-03-07 15:18:08 +0100

    Add view and method in controller to save a new entity

--- src/Snowtricks/PlatformBundle/Controller/TrickController.php
+++ src/Snowtricks/PlatformBundle/Controller/TrickController.php
@@ -4,6 +4,8 @@ namespace Snowtricks\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Snowtricks\PlatformBundle\Entity\Trick;
+use Snowtricks\PlatformBundle\Form\TrickType;
 
 class TrickController extends Controller
 {
@@ -30,6 +32,32 @@ class TrickController extends Controller
 
 		return $this->render('view.html.twig', array(
 			'trick' => $trick
+		));
+	}
+
+	public function addAction(Request $request)
+	{
+		$trick = new Trick();
+		$form = $this->get('form.factory')->create(TrickType::class, $trick);
+
+		if ($request->isMethod('POST')) {
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$trick->setSlug('Test');
+				$em->persist($trick);
+				$em->flush();
+
+				$request->getSession()->getFlashBag()->add('notice', 'Votre figure a bien été ajoutée !');
+				return $this->redirectToRoute('snowtricks_view', array(
+					'slug' => $trick->getSlug()
+				));
+			}
+		}
+
+		return $this->render('add.html.twig', array(
+			'form' => $form->createView()
 		));
 	}
 }
